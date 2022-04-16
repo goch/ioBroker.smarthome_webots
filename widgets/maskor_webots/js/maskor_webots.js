c01956882f01b1e17840b3415b9a9d883af09099
Christoph Gollok <christoph.gollok@gmail.com>
2022-04-16 20:59:29 +0200

    websocket communication with webots

---
+++ widgets/maskor_webots/js/maskor_webots.js
@@ -0,0 +1,73 @@
+/*
+	ioBroker.vis maskor_webots Widget-Set
+
+	version: "0.0.1"
+
+	Copyright 2022 goch christoph.gollok@alumni.fh-aachen.de
+*/
+"use strict";
+
+// add translations for edit mode
+$.extend(
+	true,
+	systemDictionary,
+	{
+		// Add your translations here, e.g.:
+		// "size": {
+		// 	"en": "Size",
+		// 	"de": "Größe",
+		// 	"ru": "Размер",
+		// 	"pt": "Tamanho",
+		// 	"nl": "Grootte",
+		// 	"fr": "Taille",
+		// 	"it": "Dimensione",
+		// 	"es": "Talla",
+		// 	"pl": "Rozmiar",
+		// 	"zh-cn": "尺寸"
+		// }
+	}
+);
+
+// this code can be placed directly in maskor_webots.html
+vis.binds["maskor_webots"] = {
+	version: "0.0.1",
+	showVersion: function () {
+		if (vis.binds["maskor_webots"].version) {
+			console.log("Version maskor_webots: " + vis.binds["maskor_webots"].version);
+			vis.binds["maskor_webots"].version = null;
+		}
+	},
+	createWidget: function (widgetID, view, data, style) {
+		var $div = $("#" + widgetID);
+		// if nothing found => wait
+		if (!$div.length) {
+			return setTimeout(function () {
+				vis.binds["maskor_webots"].createWidget(widgetID, view, data, style);
+			}, 100);
+		}
+
+		var text = "";
+		text += "OID: " + data.oid + "</div><br>";
+		text += 'OID value: <span class="maskor_webots-value">' + vis.states[data.oid + ".val"] + "</span><br>";
+		text += 'Color: <span style="color: ' + data.myColor + '">' + data.myColor + "</span><br>";
+		text += "extraAttr: " + data.extraAttr + "<br>";
+		text += "Browser instance: " + vis.instance + "<br>";
+		text += 'htmlText: <textarea readonly style="width:100%">' + (data.htmlText || "") + "</textarea><br>";
+
+		$("#" + widgetID).html(text);
+
+		// subscribe on updates of value
+		function onChange(e, newVal, oldVal) {
+			$div.find(".template-value").html(newVal);
+		}
+		if (data.oid) {
+			vis.states.bind(data.oid + ".val", onChange);
+			//remember bound state that vis can release if didnt needed
+			$div.data("bound", [data.oid + ".val"]);
+			//remember onchange handler to release bound states
+			$div.data("bindHandler", onChange);
+		}
+	}
+};
+
+vis.binds["maskor_webots"].showVersion();
