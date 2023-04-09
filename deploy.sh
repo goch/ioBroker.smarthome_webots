765c5770864ab4ecc5fe4e3d1846b0ff36a09f02
Christoph Gollok <christoph.gollok@gmail.com>
2023-04-09 22:55:53 +0200

    deploy script now uploads admin webside

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,11 @@
 #!/usr/bin/bash
 
 npm pack
-scp iobroker.maskor_webots-0.0.2.tgz pi@iobroker-expert.local:
+#scp iobroker.maskor_webots-0.0.2.tgz pi@iobroker-expert.local:
 #ssh -t pi@iobroker-expert.local 'bash -l -c  "iobroker stop maskor_webots.0 && cd /opt/iobroker && npm i ~/iobroker.maskor_webots-0.0.2.tgz"' 
+
+
+cd /opt/iobroker
+sudo npm i /home/goch/bachelor_ws/ioBroker.maskor_webots/iobroker.maskor_webots-0.0.2.tgz
+iobroker upload smarthome_webots
+
